5f65384b49707b9adf319536a1774a624e30afd7
quasilyte <quasilyte@gmail.com>
2016-10-29 18:58:19 +0300

    frame storage

---
+++ src/core/runtime/frame.c
@@ -0,0 +1,41 @@
+#include "core/runtime/frame.h"
+
+#include "cstd/stdlib.h"
+
+static byte* fp_top;
+static byte* fp_bottom;
+static byte* fp;
+
+static const int SLOT_SIZE = sizeof(void*);
+
+void frame_module_init(u64 byte_size) {
+  fp_top = malloc(byte_size);
+  fp_bottom = fp_top + byte_size;
+  fp = fp_top;
+}
+
+void frame_set_int(FrameSlot slot, i64 val) {
+  ((i64*)fp)[slot] = val;
+}
+
+void frame_set_float(FrameSlot slot, double val) {
+  ((double*)fp)[slot] = val;
+}
+
+i64 frame_get_int(FrameSlot slot) {
+  return ((i64*)fp)[slot];
+}
+
+double frame_get_float(FrameSlot slot) {
+  return ((double*)fp)[slot];
+}
+
+void frame_alloc(u32 slot_count) {
+  fp += slot_count * SLOT_SIZE;
+  assert(fp <= fp_bottom && "frame overflow");
+}
+
+void frame_dealloc(u32 slot_count) {
+  fp -= slot_count * SLOT_SIZE;
+  assert(fp >= fp_top && "frame underflow");
+}
---
+++ src/core/runtime/frame.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "core/project/project.h"
+
+typedef i32 FrameSlot;
+
+//! @brief Prepare frames; allocate @p byte_size bytes
+void frame_module_init(u64 byte_size);
+
+/*!
+ * @defgroup frame_get_<T>
+ * @brief Fetch frame variable
+ */
+//!{
+i64 frame_get_int(FrameSlot);
+double frame_get_float(FrameSlot);
+//!}
+
+/*!
+ * @defgroup frame_set_<T>
+ * @brief Store frame variable
+ */
+//!{
+void frame_set_int(FrameSlot, i64 val);
+void frame_set_float(FrameSlot, double val);
+//!}
+
+//! @brief Push new @p slot_count slots onto frame
+void frame_alloc(u32 slot_count);
+
+//! @brief Pop last @p slot_count slots from frame
+void frame_dealloc(u32 slot_count);
