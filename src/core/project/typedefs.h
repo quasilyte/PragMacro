bcf60ace00098179f91e40c0bb05d48ec0f6d8ae
quasilyte <quasilyte@gmail.com>
2016-10-29 18:46:22 +0300

    byte typedef

--- src/core/project/typedefs.h
+++ src/core/project/typedefs.h
@@ -7,8 +7,6 @@
 #include "cstd/stdint.h"
 #include "cstd/assert.h"
 
-typedef unsigned int uint;
-
 typedef int64_t i64;
 typedef int32_t i32;
 typedef int16_t i16;
@@ -18,6 +16,9 @@ typedef uint64_t u64;
 typedef uint32_t u32;
 typedef uint16_t u16;
 typedef int8_t u8;
+
+typedef unsigned int uint;
+typedef u8 byte;
 
 static_assert(
   sizeof(i64) == 8 &&
