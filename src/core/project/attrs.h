7f24383272551c4f4bc81d5b72fe41b55f6acfb0
quasilyte <quasilyte@gmail.com>
2016-10-29 18:46:47 +0300

    hot & const attrs

--- src/core/project/attrs.h
+++ src/core/project/attrs.h
@@ -1,3 +1,9 @@
 #pragma once
 
+/*!
+ * @brief Shortcuts for function attributes
+ */
+
 #define ATT_PURE __attribute__((pure))
+#define ATT_CONST __attribute__((const))
+#define ATT_HOT __attribute__((hot))
