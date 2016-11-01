be85e0fb3789ba83e0d2f61929b60449b298c052
quasilyte <quasilyte@gmail.com>
2016-11-02 00:41:06 +0300

    extended op list

--- src/lang/repr/opcode.h
+++ src/lang/repr/opcode.h
@@ -8,6 +8,7 @@
  * -- Naming hints --
  * L: local slot (1 byte)
  * I: immediate (size depends on the opcode)
+ * digits: constant (requires no explicit operand, encoded in memory)
  */
 ENUM(Opcode) {
   OP_END, //!< stop evaluation
@@ -25,4 +26,19 @@ ENUM(Opcode) {
   OP_MOVI, //!< local = imm64
 
   OP_JNZ, //!< IF (local != 0) GOTO imm16
+
+  OP_QLOAD0, //!< local1 = local2[0]
+  OP_QLOADI, //!< local1 = local2[imm8]
+  OP_QLOADL, //!< local1 = local2[local3]
+
+  OP_QSTORE00, //!< local[0] = 0
+  OP_QSTORE0I, //!< local[0] = imm64
+  OP_QSTORE0L, //!< local1[0] = local2
+  OP_QSTOREI0, //!< local[imm8] = 0
+  OP_QSTOREII, //!< local[imm8] = imm8
+  OP_QSTOREIL, //!< local1[imm8] = local2
+  OP_QSTOREL0, //!< local1[local2] = 0
+  OP_QSTORELI, //!< local1[local2] = imm8
+  OP_QSTORELL, //!< local1[local2] = local3
 };
+
--- src/lang/runtime/eval.c
+++ src/lang/runtime/eval.c
@@ -1,7 +1,9 @@
 #include "lang/runtime/eval.h"
 
-#include "lang/runtime/frame.h"
-#include "lang/runtime/pc.h"
+#include "lang/runtime/env/frame.h"
+#include "lang/runtime/env/pc.h"
+#include "lang/runtime/array.h"
+#include "lang/runtime/object.h"
 
 void handle_add1(FrameSlot dst) {
   frame_seti(dst, frame_geti(dst) + 1);
@@ -45,6 +47,54 @@ void handle_jnz(FrameSlot slot, i16 offset) {
   }
 }
 
+void handle_qload0(FrameSlot dst, FrameSlot src) {
+  frame_seti(dst, obj_qload(frame_geto(src), 0));
+}
+
+void handle_qloadi(FrameSlot dst, FrameSlot src, u8 idx) {
+  frame_seti(dst, obj_qload(frame_geto(src), idx));
+}
+
+void handle_qloadl(FrameSlot dst, FrameSlot src, FrameSlot idx) {
+  frame_seti(dst, obj_qload(frame_geto(src), frame_geti(idx)));
+}
+
+void handle_qstore00(FrameSlot dst) {
+  obj_store(frame_geto(dst), 0, 0);
+}
+
+void handle_qstore0i(FrameSlot dst, i64 val) {
+  obj_store(frame_geto(dst), 0, val);
+}
+
+void handle_qstore0l(FrameSlot dst, FrameSlot src) {
+  obj_store(frame_geto(dst), 0, frame_geti(src));
+}
+
+void handle_qstorei0(FrameSlot dst, u8 idx) {
+  obj_store(frame_geto(dst), idx, 0);
+}
+
+void handle_qstoreii(FrameSlot dst, u8 idx, i64 val) {
+  obj_store(frame_geto(dst), idx, val);
+}
+
+void handle_qstoreil(FrameSlot dst, u8 idx, FrameSlot src) {
+  obj_store(frame_geto(dst), idx, frame_geti(src));
+}
+
+void handle_qstorel0(FrameSlot dst, FrameSlot idx) {
+  obj_store(frame_geto(dst), frame_geti(idx), 0);
+}
+
+void handle_qstoreli(FrameSlot dst, FrameSlot idx, i64 val) {
+  obj_store(frame_geto(dst), frame_geti(idx), val);
+}
+
+void handle_qstorell(FrameSlot dst, FrameSlot idx, FrameSlot src) {
+  obj_store(frame_geto(dst), frame_geti(idx), frame_geti(src));
+}
+
 void eval(void) {
   #define NEXT_OP goto *handlers[pc_fetch_byte()]
   #define HANDLER1(NAME, T1) \
@@ -58,6 +108,14 @@ void eval(void) {
       T1 a = pc_fetch_##T1(); \
       T2 b = pc_fetch_##T2(); \
       handle_##NAME(a, b); \
+      NEXT_OP; \
+    }
+  #define HANDLER3(NAME, T1, T2, T3) \
+    op_##NAME: { \
+      T1 a = pc_fetch_##T1(); \
+      T2 b = pc_fetch_##T2(); \
+      T3 c = pc_fetch_##T3(); \
+      handle_##NAME(a, b, c); \
       NEXT_OP; \
     }
 
@@ -77,6 +135,20 @@ void eval(void) {
     &&op_movi,
 
     &&op_jnz,
+
+    &&op_qload0,
+    &&op_qloadi,
+    &&op_qloadl,
+
+    &&op_qstore00,
+    &&op_qstore0i,
+    &&op_qstore0l,
+    &&op_qstorei0,
+    &&op_qstoreii,
+    &&op_qstoreil,
+    &&op_qstorel0,
+    &&op_qstoreli,
+    &&op_qstorell,
   };
 
   NEXT_OP;
@@ -92,4 +164,16 @@ void eval(void) {
   HANDLER2(movi, byte, i64)
   HANDLER2(movl, byte, byte)
   HANDLER2(jnz, byte, i16)
+  HANDLER2(qload0, byte, byte)
+  HANDLER3(qloadi, byte, byte, byte)
+  HANDLER3(qloadl, byte, byte, byte)
+  HANDLER1(qstore00, byte)
+  HANDLER2(qstore0i, byte, i64)
+  HANDLER2(qstore0l, byte, byte)
+  HANDLER2(qstorei0, byte, byte)
+  HANDLER3(qstoreii, byte, byte, i64)
+  HANDLER3(qstoreil, byte, byte, byte)
+  HANDLER2(qstorel0, byte, byte)
+  HANDLER3(qstoreli, byte, byte, i64)
+  HANDLER3(qstorell, byte, byte, byte)
 }
