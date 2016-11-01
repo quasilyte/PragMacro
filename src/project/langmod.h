568ceff11dd3e778ed6d749d60a905d0873b42c6
quasilyte <quasilyte@gmail.com>
2016-11-01 22:49:49 +0300

    dev_assert macro

--- src/project/langmod.h
+++ src/project/langmod.h
@@ -18,3 +18,13 @@
 #define ENUM(E) \
   typedef enum E E; \
   enum E
+
+/*!
+ * @brief Assertions those value is almost 100% in code documentation
+ * @note Turn off them before profiling! They really can make things slow
+ */
+#ifdef DEV_MODE
+  #define dev_assert(X) assert(X)
+#else
+  #define dev_assert(X) (void)0
+#endif
