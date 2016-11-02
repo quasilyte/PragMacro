f96865af6b54bbdb7924f4fbd833ede8d68ef0a3
quasilyte <quasilyte@gmail.com>
2016-11-02 11:47:37 +0300

    removing specialized (optimized) op variants; they can be added later

--- src/lang/repr/opcode.h
+++ src/lang/repr/opcode.h
@@ -13,32 +13,23 @@
 ENUM(Opcode) {
   OP_END, //!< stop evaluation
 
-  OP_ADD1, //!< local += 1
   OP_ADDI, //!< local += imm32
   OP_ADDL, //!< local += local
 
-  OP_SUB1, //!< local -= 1
   OP_SUBI, //!< local -= imm32
   OP_SUBL, //!< local -= local
 
-  OP_MOV0, //!< local = 0
   OP_MOVL, //!< local = local
   OP_MOVI, //!< local = imm64
 
   OP_JNZ, //!< IF (local != 0) GOTO imm16
 
-  OP_QLOAD0, //!< local1 = local2[0]
   OP_QLOADI, //!< local1 = local2[imm8]
   OP_QLOADL, //!< local1 = local2[local3]
 
-  OP_QSTORE00, //!< local[0] = 0
-  OP_QSTORE0I, //!< local[0] = imm64
-  OP_QSTORE0L, //!< local1[0] = local2
-  OP_QSTOREI0, //!< local[imm8] = 0
-  OP_QSTOREII, //!< local[imm8] = imm8
+  OP_QSTOREII, //!< local[imm8] = imm64
   OP_QSTOREIL, //!< local1[imm8] = local2
-  OP_QSTOREL0, //!< local1[local2] = 0
-  OP_QSTORELI, //!< local1[local2] = imm8
+  OP_QSTORELI, //!< local1[local2] = imm64
   OP_QSTORELL, //!< local1[local2] = local3
 };
 
--- src/lang/runtime/eval.c
+++ src/lang/runtime/eval.c
@@ -5,10 +5,6 @@
 #include "lang/runtime/array.h"
 #include "lang/runtime/object.h"
 
-void handle_add1(FrameSlot dst) {
-  frame_seti(dst, frame_geti(dst) + 1);
-}
-
 void handle_addi(FrameSlot dst, i32 val) {
   frame_seti(dst, frame_geti(dst) + val);
 }
@@ -17,20 +13,12 @@ void handle_addl(FrameSlot dst, FrameSlot src) {
   frame_seti(dst, frame_geti(dst) + frame_geti(src));
 }
 
-void handle_sub1(FrameSlot dst) {
-  frame_seti(dst, frame_geti(dst) - 1);
-}
-
 void handle_subi(FrameSlot dst, i32 val) {
   frame_seti(dst, frame_geti(dst) - val);
 }
 
 void handle_subl(FrameSlot dst, FrameSlot src) {
   frame_seti(dst, frame_geti(dst) - frame_geti(src));
-}
-
-void handle_mov0(FrameSlot dst) {
-  frame_seti(dst, 0);
 }
 
 void handle_movi(FrameSlot dst, i64 val) {
@@ -47,10 +35,6 @@ void handle_jnz(FrameSlot slot, i16 offset) {
   }
 }
 
-void handle_qload0(FrameSlot dst, FrameSlot src) {
-  frame_seti(dst, obj_qload(frame_geto(src), 0));
-}
-
 void handle_qloadi(FrameSlot dst, FrameSlot src, u8 idx) {
   frame_seti(dst, obj_qload(frame_geto(src), idx));
 }
@@ -59,32 +43,12 @@ void handle_qloadl(FrameSlot dst, FrameSlot src, FrameSlot idx) {
   frame_seti(dst, obj_qload(frame_geto(src), frame_geti(idx)));
 }
 
-void handle_qstore00(FrameSlot dst) {
-  obj_store(frame_geto(dst), 0, 0);
-}
-
-void handle_qstore0i(FrameSlot dst, i64 val) {
-  obj_store(frame_geto(dst), 0, val);
-}
-
-void handle_qstore0l(FrameSlot dst, FrameSlot src) {
-  obj_store(frame_geto(dst), 0, frame_geti(src));
-}
-
-void handle_qstorei0(FrameSlot dst, u8 idx) {
-  obj_store(frame_geto(dst), idx, 0);
-}
-
 void handle_qstoreii(FrameSlot dst, u8 idx, i64 val) {
   obj_store(frame_geto(dst), idx, val);
 }
 
 void handle_qstoreil(FrameSlot dst, u8 idx, FrameSlot src) {
   obj_store(frame_geto(dst), idx, frame_geti(src));
-}
-
-void handle_qstorel0(FrameSlot dst, FrameSlot idx) {
-  obj_store(frame_geto(dst), frame_geti(idx), 0);
 }
 
 void handle_qstoreli(FrameSlot dst, FrameSlot idx, i64 val) {
@@ -122,31 +86,22 @@ void eval(void) {
   static const void* handlers[] = {
     &&op_end,
 
-    &&op_add1,
     &&op_addi,
     &&op_addl,
 
-    &&op_sub1,
     &&op_subi,
     &&op_subl,
 
-    &&op_mov0,
     &&op_movl,
     &&op_movi,
 
     &&op_jnz,
 
-    &&op_qload0,
     &&op_qloadi,
     &&op_qloadl,
 
-    &&op_qstore00,
-    &&op_qstore0i,
-    &&op_qstore0l,
-    &&op_qstorei0,
     &&op_qstoreii,
     &&op_qstoreil,
-    &&op_qstorel0,
     &&op_qstoreli,
     &&op_qstorell,
   };
@@ -154,26 +109,17 @@ void eval(void) {
   NEXT_OP;
 
   op_end: return;
-  HANDLER1(add1, byte)
   HANDLER2(addi, byte, i32)
   HANDLER2(addl, byte, byte)
-  HANDLER1(sub1, byte)
   HANDLER2(subi, byte, i32)
   HANDLER2(subl, byte, byte)
-  HANDLER1(mov0, byte)
   HANDLER2(movi, byte, i64)
   HANDLER2(movl, byte, byte)
   HANDLER2(jnz, byte, i16)
-  HANDLER2(qload0, byte, byte)
   HANDLER3(qloadi, byte, byte, byte)
   HANDLER3(qloadl, byte, byte, byte)
-  HANDLER1(qstore00, byte)
-  HANDLER2(qstore0i, byte, i64)
-  HANDLER2(qstore0l, byte, byte)
-  HANDLER2(qstorei0, byte, byte)
   HANDLER3(qstoreii, byte, byte, i64)
   HANDLER3(qstoreil, byte, byte, byte)
-  HANDLER2(qstorel0, byte, byte)
   HANDLER3(qstoreli, byte, byte, i64)
   HANDLER3(qstorell, byte, byte, byte)
 }
