fbe8896907820b9eb5112c4d833e1817a263a728
quasilyte <quasilyte@gmail.com>
2016-11-03 19:27:35 +0300

    updated lang sdk

--- src/api/builtin.h
+++ src/api/builtin.h
@@ -10,32 +10,57 @@
  * #TODO: put links here
  */
 
-//! @brief Type of all user defined structs
-typedef void* Struct;
+/*!
+ * @section Keywords
+ */
+
+#ifndef NULL
+  #define NULL ((void*)0)
+#endif
+
+#define each(KEY, VALUE) \
+  for (KEY;;) for (VALUE;)
+
+#define each_value(VALUE) \
+  for (VALUE;)
+
+#define each_key(KEY) \
+  for (KEY;)
+
+#define in ;((void*)0) == (void*)&
+
+/*!
+ * @section Token related
+ */
 
 typedef struct Token Token;
 struct Token {/* Opaque */};
 
 /*!
- * @section Array container
+ * @section Struct related
  */
 
-//! @brief Mono type elems; indexed by {int} keys; Ordered & contiguous
+//! @brief Untyped struct. "Parent" type of any struct
+typedef void* Struct;
+
+/*!
+ * @brief Type specialized struct; fully compatible with untyped Struct
+ */
+#define Struct(T) struct T*
+
+#define make(T, ...) NULL
+
+/*!
+ * @section Array related
+ */
+
+//! @brief Untyped Array. "Parent" type of any array
 typedef void* Array;
 
 /*!
- * @defgroup <T>Array
  * @brief Type specialized arrays; fully compatible with untyped Array
- *
- * Advantages of using specialized arrays:
- * 1) They make API more specific and clear
- * 2) Its possible to use native indexing with them
  */
-//!{
-typedef int* IntArray;
-typedef float* FloatArray;
-typedef char* CharArray;
-//!}
+#define Array(T) T*
 
 //! @brief Array length
 int len(Array);
@@ -79,96 +104,6 @@ void sort_asc(Array);
 void reverse(Array);
 
 /*!
- * @defgroup Ts
- * @brief Create a new array from passed elements
- */
-//!{
-Array ints(int elem, ...);
-Array floats(float elem, ...);
-Array chars(char elem, ...);
-Array tokens(Token elem, ...);
-Array arrays(Array elem, ...);
-Array structs(Struct elem, ...);
-//!}
-
-/*!
- * @defgroup new_Ts
- * @brief Create a new, empty array; Fills {len} elements with zero value
- */
-//!{
-Array new_ints(int len);
-Array new_floats(int len);
-Array new_chars(int len);
-Array new_tokens(int len);
-Array new_arrays(int len);
-Array new_structs(int len);
-//!}
-
-/*!
- * @defgroup set_T
- * @brief Change single array element value; {idx} should be in array bounds
- */
-//!{
-void set_int(Array, int idx, int val);
-void set_float(Array, int idx, float val);
-void set_char(Array, int idx, char val);
-void set_token(Array, int idx, Token val);
-void set_array(Array, int idx, Array val);
-void set_struct(Array, int idx, Array val);
-//!}
-
-/*!
- * @defgroup push_T
- * @brief Add {val} to the end of {Array}; increases len
- */
-//!{
-void push_int(Array, int val);
-void push_float(Array, float val);
-void push_char(Array, char val);
-void push_token(Array, Token val);
-void push_array(Array, Array val);
-void push_struct(Array, Struct val);
-//!}
-
-/*!
- * @defgroup get_T
- * @brief Get value stored inside {Array} in {idx} cell
- */
-//!{
-int get_int(Array, int idx);
-float get_float(Array, int idx);
-char get_char(Array, int idx);
-Token get_token(Array, int idx);
-Array get_array(Array, int idx);
-Struct get_struct(Array, int idx);
-//!}
-
-/*!
- * @defgroup pop_T
- * @brief Remove and return last {Array} element; decreases len
- */
-//!{
-int pop_int(Array);
-float pop_float(Array);
-char pop_char(Array);
-Token pop_token(Array);
-Array pop_array(Array);
-Struct pop_struct(Array);
-//!}
-
-/*!
- * @defgroup find_T_index
- * @brief Seeks for {needle} index inside {Array}; -1 when not found
- */
-//!{
-int find_int_index(Array, int needle);
-int find_float_index(Array, float needle);
-int find_char_index(Array, char needle);
-int find_token_index(Array, Token needle);
-int find_array_index(Array, Array needle);
-//!}
-
-/*!
  * @defgroup is_Ts
  * @brief Returns 1 if array is specifically tagged, 0 otherwise
  */
@@ -176,9 +111,80 @@ int find_array_index(Array, Array needle);
 int is_ints(Array);
 int is_floats(Array);
 int is_chars(Array);
-int is_tokens(Array);
 int is_arrays(Array);
 int is_structs(Array);
+//!}
+
+/*!
+ * @defgroup Ts
+ * @brief Create a new array from passed elements
+ */
+//!{
+Array(int) ints(int elem, ...);
+Array(float) floats(float elem, ...);
+Array(char) chars(char elem, ...);
+Array(Array) arrays(Array elem, ...);
+Array(Struct) structs(Struct elem, ...);
+//!}
+
+/*!
+ * @defgroup new_Ts
+ * @brief Create a new, empty array; Fills {len} elements with zero value
+ */
+//!{
+Array(int) new_ints(int len);
+Array(float) new_floats(int len);
+Array(char) new_chars(int len);
+Array(Array) new_arrays(int len);
+Array(Struct) new_structs(int len);
+//!}
+
+/*!
+ * @defgroup set_T
+ * @brief Change single array element value; {idx} should be in array bounds
+ */
+//!{
+void set_int(Array(int), int idx, int val);
+void set_float(Array(float), int idx, float val);
+void set_char(Array(char), int idx, char val);
+void set_array(Array(Array), int idx, Array val);
+void set_struct(Array(Struct), int idx, Array val);
+//!}
+
+/*!
+ * @defgroup push_T
+ * @brief Add {val} to the end of {Array}; increases len
+ */
+//!{
+void push_int(Array(int), int val);
+void push_float(Array(float), float val);
+void push_char(Array(char), char val);
+void push_array(Array(Array), Array val);
+void push_struct(Array(Struct), Struct val);
+//!}
+
+/*!
+ * @defgroup get_T
+ * @brief Get value stored inside {Array} in {idx} cell
+ */
+//!{
+int get_int(Array(int), int idx);
+float get_float(Array(float), int idx);
+char get_char(Array(char), int idx);
+Array get_array(Array(Array), int idx);
+Struct get_struct(Array(Struct), int idx);
+//!}
+
+/*!
+ * @defgroup pop_T
+ * @brief Remove and return last {Array} element; decreases len
+ */
+//!{
+int pop_int(Array(int));
+float pop_float(Array(float));
+char pop_char(Array(char));
+Array pop_array(Array(Array));
+Struct pop_struct(Array(Struct));
 //!}
 
 /*!
@@ -193,28 +199,6 @@ int is_structs(Array);
 void print_int(int);
 void print_float(float);
 void print_char(char);
-void print_token(Token);
 void print_array(Array);
 void print_struct(Struct);
 //!}
-
-/*!
- * @section Keywords
- */
-
-//! @brief Define new struct type
-#define type(NAME) \
-  typedef struct NAME NAME; \
-  struct NAME
-
-#define each(KEY, VALUE) \
-  for (KEY;;) for (VALUE;)
-
-#define each_value(VALUE) \
-  for (VALUE;)
-
-#define each_key(KEY) \
-  for (KEY;)
-
-#define in ;((void*)0) == (void*)&
-
