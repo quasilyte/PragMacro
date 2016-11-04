14329fbb56ec4e29c509407371912b3f3e11efcb
quasilyte <quasilyte@gmail.com>
2016-11-04 19:44:06 +0300

    func also should have explicit id member field

--- src/lang/symbol/func.h
+++ src/lang/symbol/func.h
@@ -4,6 +4,7 @@
 
 //! @brief Function info
 STRUCT(Func) {
+  u32 id;
+  u32 frame_size; //!< Bytes on frame needed
   u64 addr; //!< Absolute address inside ProgramCounter
-  uint frame_size; //!< Bytes on frame needed
 };
