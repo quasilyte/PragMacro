9d7e775480a2bc04183641139c4d42f88586a1d5
quasilyte <quasilyte@gmail.com>
2016-11-05 00:11:17 +0300

    removed pointless comment

--- src/project/langmod.h
+++ src/project/langmod.h
@@ -19,10 +19,6 @@
   typedef enum E E; \
   enum E
 
-/*!
- * @brief Assertions those value is almost 100% in code documentation
- * @note Turn off them before profiling! They really can make things slow
- */
 #ifdef DEV_MODE
   #define dev_assert(X) assert(X)
 #else
